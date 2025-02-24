e4c3e646f197196dfc4541d1812cb14dc85ebb49
Markos Calderon <163056970+MKVolk@users.noreply.github.com>
2025-02-24 00:24:47 -0500

    Create index.js
    Upload the script from local IDE

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+function menuBarOpen (){
+  document.getElementById("navBar").style.height="60px";
+  
+  document.getElementById("pokediv").style.paddingBottom="60px";
+  document.getElementById("menuOpen").style.display="none";
+  document.getElementById("menuClose").style.display="inline";
+}
+function menuBarClose (){
+  document.getElementById("navBar").style.height="0px";
+  document.getElementById("navBar").style.overflowY="hidden";
+  document.getElementById("pokediv").style.paddingBottom="0px";
+  document.getElementById("menuOpen").style.display="inline";
+  document.getElementById("menuClose").style.display="none";
+}
+function overShadow (){
+  document.getElementById("navBar").style.overflowY="overflow";
+}
+
+function css1Function (){
+  document.getElementById("styleLink").href="../a/css/glowy.css";
+}
+function css2Function (){
+  documnet.getElementById("styleLink").href="../a/css/glowy.css";
+}
+function css3Function (){
+  document.getElementById("styleLink").href="../a/css/glowy.css";
+}
